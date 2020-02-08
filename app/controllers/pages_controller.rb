9019e31b73e9edc6f8a2285751168cd05bb84c4e
subishaos <subisha.os@xtrastaff.com>
2020-02-08 16:08:51 +0530

    New project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def chage_status
+    debugger;
+    x = Room.last
+    x.status = !x.status
+    x.save
+    render json: {room_status: 1}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/index'
   root 'pages#index'
+  get 'chage_status' ,  to: 'pages#chage_status'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
